ccc1cbe1c075f68ba09debaa49a0a09873ae862b
brake <riaconoo@icloud.com>
2022-08-09 12:15:44 +1000

    created models and relationships in index.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 const routes = require('./routes');
 // import sequelize connection
 
@@ -10,7 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
